78458ab4997035af7294d74cdb52a5a9dbf13091
Karla Beatriz <studieskarla@gmail.com>
2025-08-16 16:28:56 -0300

    Create desafio074.py

---
+++ modulo03/desafio074.py
@@ -0,0 +1,16 @@
+import random
+# Progrma que gera 5 numeros aleatórios e coloca em tuplas
+# mostrar a lista desses numeros
+# indicar maior e menor valor da tupla
+
+# gera uma lista vazia (opcional)
+cinco_numeros = []
+#[ ... for _ in range(5)] → é como dizer: “faça isso 5 vezes
+cinco_numeros = [random.randint(1,50) for _ in range(5)]
+
+maior_valor = max(cinco_numeros)
+menor_valor = min(cinco_numeros)
+
+print(f"Os números gerados foram: {cinco_numeros}")
+print(f"O MAIOR valor gerado foi {maior_valor} e o MENOR valor gerado foi {menor_valor}")
+
