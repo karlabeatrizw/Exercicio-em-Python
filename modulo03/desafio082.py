13ae88fdc2c5aa7a656576ac84a3b36dfccb9e33
Karla Beatriz <studieskarla@gmail.com>
2025-08-22 15:14:43 -0300

    Update desafio082.py

--- modulo03/desafio082.py
+++ modulo03/desafio082.py
@@ -6,20 +6,28 @@ impares = []
 
 while True:
     numeros = int(input(f'Digite um valor: '))
-
-    continuar = str(input(f'Deseja continuar S/N:')).lower
+    valores.append(numeros)
+    
+    continuar = str(input(f'Deseja continuar S/N:')).lower()
 
     if continuar == 'n':
         print('Você finalizou a lista')
         break
     
-    valores.append(numeros)
-    
+
     if numeros % 2 == 0:
         pares.append(numeros)
 
     if numeros % 2 != 0:
         impares.append(numeros)
+
+print('______________________________________________________________________')
+print(f'A lista gerada foi {valores}')
+print('______________________________________________________________________')
+print(f'Os numeros pares foram {pares}')
+print('______________________________________________________________________')
+print(f'Os numeros impares foram {impares}')
+
 
 
 
