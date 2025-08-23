e32864b686983aa3cde14386cbb58798ccafdf7f
Karla Beatriz <studieskarla@gmail.com>
2025-08-23 15:43:35 -0300

    Create desafio083.py

---
+++ modulo03/desafio083.py
@@ -0,0 +1,24 @@
+expressao = str(input('Digite a expressão: '))
+pilha = []
+cont = 0
+cont2 = 0
+for simbolo in expressao:
+    if simbolo == '(':
+        cont += 1
+
+for simbolo2 in expressao:    
+    if simbolo2 == ')':
+        cont2 += 1
+
+
+
+if cont == cont2:
+        print('Todos os parenteses tem fechamento')
+if cont > cont2 or cont2 > cont:
+     num_faltantes = cont - cont2
+
+if num_faltantes < 0:
+        num_faltantes*(-1)
+
+else:
+    print(f'Há {num_faltantes} parentese(s) sem fechamento')
